4b6727a6b6a56b8af6b4718e52dfe1b6b007c7b2
Jan Dwight Monreal <monreal.jandwight@gmail.com>
2023-03-12 21:19:58 +0800

    Protection for defaultLayout implemented

--- psyportal/src/main.jsx
+++ psyportal/src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { ContextProvider } from './contect/contextprovider'
 import './index.css'
 import router from './router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ContextProvider>
+      <RouterProvider router={router} />
+    </ContextProvider>
   </React.StrictMode>,
 )
+
+
