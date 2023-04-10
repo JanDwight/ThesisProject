b203b983f73725fe3deccad10366b7d144c7af7e
JanDwight <monreal.jandwight@gmail.com>
2023-04-10 15:29:37 +0800

    Evalluatiion Page Partially Done

---
+++ psyportal/src/components/Evalluation/EVButton.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import EVModal from './EVModal';
+
+function EVButton() {
+    
+    const [showEVModal, setShowEVModal] = useState(false);
+    const handleOnClose = () => setShowEVModal(false);
+
+  return (
+    <div className="bg-green-600 rounded h-10 mt-3">
+                    <button onClick={() => setShowEVModal(true)} className='flex  items-center h-full w-full'>
+                        <div className='grid grid-cols-3 gap-x-[270px]'>
+                            <div>IT101</div>
+                            <div>Intro to IT</div>
+                            <div>Sir X</div>
+                        </div>
+                        
+                        {/*Not Working And I Dont Know Why */}
+                        <EVModal onClose={handleOnClose} visible={showEVModal}/>
+                    </button>
+                </div>
+  )
+}
+
+export default EVButton
