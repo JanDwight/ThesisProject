b014b07e80cb16452379b0906d74a0d2573ec778
JanDwight <monreal.jandwight@gmail.com>
2023-04-20 12:01:13 +0800

    Accordion Implemented

--- psyportal/src/components/DefaultLayout.jsx
+++ psyportal/src/components/DefaultLayout.jsx
@@ -18,51 +18,42 @@ const navigation = [
 
 function DefaultLayout() {
     return (
-        <div className='flex flex-row justify-center mx-auto max-w-screen-2xl'>
+        <div className='flex flex-row justify-center mx-auto max-w-screen-2xl '>
             <div className='h-screen bg-green-600 hidden sm:block w-52'>
               
-            <div className='ml-20 h-1 w-10 '>
-              <div className='mb-5 mt-5'>
-                <NavLink to={'/userprofile'}>
-                  <img src={user} alt="" />
-                </NavLink>
-              </div>
-                  
-                    
+              <div className='ml-20 h-1 w-10 '>
+                <div className='mb-5 mt-5'>
+                  <NavLink to={'/userprofile'}>
+                    <img src={user} alt="" />
+                  </NavLink>
+                </div>
     
-                  <button>
+                <button>
                   <img src={bell} alt="" />
-                  </button>
-                  
-                </div>
+                </button> 
+              </div>
     
-                <div className="flex flex-col mt-[150px]">
-                    {/* This will handle the clicks on the buttons*/}
-                    {navigation.map((item) => (
-                        <NavLink
-                            key={item.name}
-                            to={item.to}
-                            className={({isActive}) => classNames(
-                            isActive ? 'bg-green-300 text-black' : 'text-black hover:bg-green-300 hover:text-black',
-                            'rounded-md px-10 py-2 text-medium font-bold')}>
-                            {item.name}
-                        </NavLink>
-                    ))}
-                </div>
-    
-                
-                
+              <div className="flex flex-col mt-[150px]">
+                {/* This will handle the clicks on the buttons*/}
+                {navigation.map((item) => (
+                  <NavLink
+                    key={item.name}
+                    to={item.to}
+                    className={({isActive}) => classNames(
+                    isActive ? 'bg-green-300 text-black' : 'text-black hover:bg-green-300 hover:text-black',
+                    'rounded-md px-10 py-2 text-medium font-bold')}>
+                    {item.name}
+                  </NavLink>
+                ))}
+              </div>
             </div>
             
-            {/**For the newsfeed column. Loads the code from Home.jsx */}
+            {/*Loads the content on the right side of the screen */}
             <div className='w-full h-100% bg-green-300 sm:ml-5 max-w-5xl h-fit'>
-    
               <div >
                 <Outlet />
               </div >
-    
             </div>
-    
         </div>
       )
 }
---
+++ psyportal/src/components/Manage Accounts/Collapsible.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Collapsible =()=>{
+    return(
+      <div>
+        <h1>
+          Students
+        </h1>
+      </div>
+    )
+  }
+
+export default Collapsible
